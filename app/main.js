ac238088a1b66518f75144e7a113cb0c74eda665
joel <joel@burgerjoe.dev>
2024-09-25 09:47:51 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,23 +1,51 @@
-import { open } from "fs/promises";
+import { open, stat } from 'fs/promises';
+import path from 'path';
 
-const databaseFilePath = process.argv[2];
+const databaseFile = process.argv[2];
 const command = process.argv[3];
 
-if (command === ".dbinfo") {
-  const databaseFileHandler = await open(databaseFilePath, "r");
+async function fetchPageType(databaseFileHandler, page, pageSize) {
+  const offset = page * pageSize;
 
   const { buffer } = await databaseFileHandler.read({
+    length: pageSize,
+    position: offset,
+    buffer: Buffer.alloc(pageSize),
+  });
+
+  return buffer.readInt8(0);
+}
+
+if (command === '.dbinfo') {
+  const filePath = path.join(process.cwd(), databaseFile);
+  // console.log(`filePath ${filePath}`);
+  const databaseFileHandler = await open(filePath, 'r');
+
+  const { buffer: fileHeader } = await databaseFileHandler.read({
     length: 100,
     position: 0,
     buffer: Buffer.alloc(100),
   });
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  console.log("Logs from your program will appear here!");
+  const fileStats = await stat(filePath);
+  const fileSize = fileStats.size;
+  //console.log(`Total file size: ${fileSize} bytes`);
 
-  // Uncomment this to pass the first stage
-  const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
+  const pageSize = fileHeader.readUInt16BE(16); // page size is 2 bytes starting at offset 16
   console.log(`database page size: ${pageSize}`);
+
+  const totalNumberOfPages = fileSize / pageSize;
+  //console.log(`total number of pages: ${totalNumberOfPages}`);
+
+  let tableCount = 0;
+  for (let page = 1; page <= totalNumberOfPages; page++) {
+    const pageType = await fetchPageType(databaseFileHandler, page, pageSize);
+    if (pageType === 13 || pageType === 5) {
+      tableCount++;
+    }
+    //console.log(`Page ${page} type: ${pageType}`);
+  }
+  console.log(`number of tables: ${tableCount}`);
 } else {
   throw `Unknown command ${command}`;
 }
