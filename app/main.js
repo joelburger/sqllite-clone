6812ce47be077a82dc643c52ee54f2e78e31359d
joel <joel@burgerjoe.dev>
2024-09-28 20:01:55 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,17 +1,13 @@
+// Refer to https://www.sqlite.org/fileformat.html
+
 import { open, stat } from 'fs/promises';
 import path from 'path';
 
 const databaseFile = process.argv[2];
 const command = process.argv[3];
 
-async function parsePageHeader(databaseFileHandler, page, pageSize) {
+function parsePageHeader(buffer, page, pageSize) {
   const offset = page === 0 ? 100 : 0;
-
-  const { buffer } = await databaseFileHandler.read({
-    length: pageSize,
-    position: page * pageSize,
-    buffer: Buffer.alloc(pageSize),
-  });
 
   const pageType = buffer.readInt8(0 + offset);
   const startFreeBlock = buffer.readUInt16BE(1 + offset);
@@ -19,58 +15,61 @@ async function parsePageHeader(databaseFileHandler, page, pageSize) {
   const startCellContentArea = buffer.readUInt16BE(5 + offset);
   const numberOfFragmentedFreeBytes = buffer.readInt8(7 + offset);
 
-  console.log('page', page);
-  console.log('pageType', pageType);
-  console.log('startFreeBlock', startFreeBlock);
-  console.log('numberOfCells', numberOfCells);
-  console.log('startCellContentArea', startCellContentArea);
-  console.log('numberOfFragmentedFreeBytes', numberOfFragmentedFreeBytes);
+  return { pageType, numberOfCells, startCellContentArea };
+}
 
-  const start = startCellContentArea;
-  const end = pageSize;
+function convertToVarInt(value) {
+  return (value - 13) / 2;
+}
 
-  const cellContent = buffer.subarray(start, end);
+function parseCellContent(buffer, numberOfCells, startCellContentArea, pageSize) {
+  const cellContent = buffer.subarray(startCellContentArea, pageSize);
 
-  //console.log(`cellContent ${start}-${end}`, cellContent.toString('utf8'));
-
-  console.log('total size', cellContent.length);
+  const tableNames = [];
 
   let index = 0;
-  let recordCount = 0;
 
-  while (recordCount < numberOfCells) {
-    // read record size
+  for (let i = 0; i < numberOfCells; i++) {
     const recordSize = cellContent[index];
     const rowId = cellContent[index + 1];
     const recordHeaderSize = cellContent[index + 2];
-    const schemaTypeSize = cellContent[index + 3];
-    const schemaSize = cellContent[index + 4];
-    const tableNameSize = cellContent[index + 5];
+    const schemaTypeSize = convertToVarInt(cellContent[index + 3]);
+    const schemaSize = convertToVarInt(cellContent[index + 4]);
+    const tableNameSize = convertToVarInt(cellContent[index + 5]);
 
-    console.log('>>>>>>>>>>>>> recordSize', recordSize);
-    console.log('>>>>>>>>>>>>> rowId', rowId);
-    console.log('>>>>>>>>>>>>> recordHeaderSize', recordHeaderSize);
-    console.log('>>>>>>>>>>>>> schemaTypeSize', (schemaTypeSize - 13) / 2);
-    console.log('>>>>>>>>>>>>> schemaSize', (schemaSize - 13) / 2);
-    console.log('>>>>>>>>>>>>> tableNameSize', (tableNameSize - 13) / 2);
+    // calculate table name start position
+    // first 2 bytes are allocated for the record size and row ID
+    const tableNameStartPosition = index + 2 + recordHeaderSize + schemaTypeSize + schemaSize;
 
-    // print record
-    console.log('>>>>>>>>>>>>> record', cellContent.subarray(index, index + recordSize + 2).toString('utf8'));
+    const tableName = cellContent
+      .subarray(tableNameStartPosition, tableNameStartPosition + tableNameSize)
+      .toString('utf8');
+    tableNames.push(tableName);
 
     // advance index to next record
     index += recordSize + 2;
-
-    // increment record counter
-    recordCount++;
   }
+
+  return { tableNames };
+}
+
+async function parsePage(databaseFileHandler, page, pageSize) {
+  // fetch page data
+  const { buffer } = await databaseFileHandler.read({
+    length: pageSize,
+    position: page * pageSize,
+    buffer: Buffer.alloc(pageSize),
+  });
+
+  const { pageType, numberOfCells, startCellContentArea } = parsePageHeader(buffer, page, pageSize);
+
+  const { tableNames } = parseCellContent(buffer, numberOfCells, startCellContentArea, pageSize);
 
   // size = 120, hexdump -C sample.db -s 3779 -n 122
   // size = 80, hexdump -C sample.db -s 3901 -n 82
   // size = 111, hexdump -C sample.db -s 3983 -n 113
 
-  console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-
-  return { pageType, startCellContentArea, numberOfCells };
+  return { pageType, tableNames };
 }
 
 async function parseFileHeader(databaseFileHandler) {
@@ -89,26 +88,26 @@ async function parseFileHeader(databaseFileHandler) {
   };
 }
 
+// read database file
+const filePath = path.join(process.cwd(), databaseFile);
+const databaseFileHandler = await open(filePath, 'r');
+
+// parse file header
+const { pageSize, totalNumberOfPages } = await parseFileHeader(databaseFileHandler);
+
+// retrieve table information from the first page
+const { pageType, tableNames } = await parsePage(databaseFileHandler, 0, pageSize);
+
 if (command === '.dbinfo') {
-  // Refer to https://www.sqlite.org/fileformat.html
-
-  // read database file
-  const filePath = path.join(process.cwd(), databaseFile);
-  const databaseFileHandler = await open(filePath, 'r');
-
-  // parse file header
-  const { pageSize, totalNumberOfPages } = await parseFileHeader(databaseFileHandler);
   console.log(`database page size: ${pageSize}`);
-
-  let tableCount = 0;
-  for (let page = 1; page < totalNumberOfPages; page++) {
-    const { pageType } = await parsePageHeader(databaseFileHandler, page, pageSize);
-    if (pageType === 13 || pageType === 5) {
-      tableCount++;
-    }
-  }
-  console.log(`number of tables: ${tableCount}`);
+  console.log(`number of tables: ${tableNames.length}`);
 } else if (command === '.tables') {
+  // list table names but exclude the internal schema table
+  const userTableNames = tableNames
+    .filter((tableName) => tableName !== 'sqlite_sequence')
+    .sort()
+    .join(' ');
+  console.log(userTableNames);
 } else {
   throw `Unknown command ${command}`;
 }
